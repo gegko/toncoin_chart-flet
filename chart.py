a6ccc3c42be9e5d2299123b4a780320ad006e944
gegko <ruqx01@gmail.com>
2023-10-03 01:15:30 +0300

    repo init

---
+++ chart.py
@@ -0,0 +1,86 @@
+import requests
+import flet as ft
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+from_str = (datetime.today() - timedelta(days=335)) \
+    .timestamp().__int__().__str__()
+to_str = datetime.today() \
+    .timestamp().__int__().__str__()
+
+base_url = 'https://api.coingecko.com/api/v3/'
+prices_url = 'coins/the-open-network/market_chart/range/'
+headers = {'accept': 'application/json'}
+params = {
+    "id": "the-open-network",
+    "vs_currency": "usd",
+    "from": from_str,
+    "to": to_str,
+}
+
+r = requests.get(
+    base_url + prices_url,
+    params=params,
+    headers=headers
+)
+yearly_data = r.json()
+dt = [datetime.fromtimestamp(dt / 1000).date() \
+      .strftime('%b') for dt, _ in yearly_data['prices']]
+data_frame = pd.DataFrame(dt, columns=['month']) \
+    .drop_duplicates(keep='first')
+date_data = zip(
+    [round(i, -1) for i in data_frame.index.to_list()],
+    data_frame.month.to_list()
+)
+
+labels = [
+    ft.ChartAxisLabel(
+                    value=value,
+                    label=ft.Container(
+                        ft.Text(
+                            text,
+                            size=12,
+                            weight=ft.FontWeight.BOLD,
+                            color=ft.colors.with_opacity(
+                                0.5, ft.colors.ON_SURFACE
+                            ),
+                        ),
+                        margin=ft.margin.only(top=10),
+                    ),
+                )
+    for value, text in date_data
+]
+
+price_data = [round(price, 5) for _, price in yearly_data['prices']]
+
+def chart():
+    return ft.LineChart(
+        tooltip_bgcolor=ft.colors.WHITE,
+        min_y=int(min(price_data)),
+        max_y=int(max(price_data)),
+        min_x=0,
+        max_x=len(price_data) - 1,
+        expand=True,
+        bottom_axis=ft.ChartAxis(
+            labels=labels,
+            labels_size=32,
+        ),
+        height=130,
+        width=1200,
+        data_series=[
+            ft.LineChartData(
+                data_points=None,
+                color=ft.colors.BLUE_700,
+                curved=True,
+                below_line_gradient=ft.LinearGradient(
+                    begin=ft.alignment.top_center,
+                    end=ft.alignment.bottom_center,
+                    colors=[
+                        ft.colors.with_opacity(0.55, ft.colors.BLUE_700),
+                        "#f6f9fe"
+                    ]
+                )
+            ),
+        ],
+    )
---
+++ main.py
@@ -0,0 +1,64 @@
+import time
+import flet as ft
+from chart import chart, price_data
+
+
+def chart_container():
+    return ft.Container(
+        alignment=ft.alignment.bottom_center,
+        bgcolor="#f6f9fe",
+        shadow=ft.BoxShadow(
+            spread_radius=.5,
+            blur_radius=.5,
+            blur_style=ft.ShadowBlurStyle.OUTER,
+            color=ft.colors.BLACK26,
+        ),
+        width=9000,
+        height=275,
+        margin=ft.margin.symmetric(horizontal=125),
+        border_radius=10,
+        content=chart(),
+    )
+
+
+PRICE_TEXT = 'Toncoin price'
+PRICE_VALUE = f"${price_data[-1]:.5f}"
+
+
+def main(page: ft.Page):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    page.add(
+        ft.Stack(
+            [
+                chart_container(),
+                ft.Column(
+                    left=150,
+                    top=20,
+                    spacing=5,
+                    controls=[
+                        ft.Text(PRICE_TEXT, color=ft.colors.GREY_600),
+                        ft.Text(
+                            PRICE_VALUE,
+                            size=20,
+                            color=ft.colors.BLACK,
+                            weight=ft.FontWeight.BOLD
+                        ),
+                    ],
+                ),
+            ],
+        )
+    )
+    line_chart = page.controls[0].controls[0].content
+    data_points = line_chart.data_series[0].data_points
+
+    for x, y in enumerate(price_data):
+        data_points.append(ft.LineChartDataPoint(x, y))
+        if x % 3 == 0:
+            line_chart.update()
+            time.sleep(0.01)
+
+    page.update()
+
+ft.app(main)
